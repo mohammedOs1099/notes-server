6136aca29e57cc85b0d6af331264ed097a7499b0
mohammedOs1099 <mohammed.osama1099@gmail.com>
2024-12-09 16:34:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import jsonServer from "json-server";
+
+const server = jsonServer.create();
+const router = jsonServer.router({
+  notes: []
+}); // قاعدة بيانات في الذاكرة
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
